291fa1fd83bd9c5bc610175a3e72cee2f6eed172
Ali Tugrul <a_tugrul@hotmail.com>
2020-01-20 10:59:02 +0300

    Add project files.

---
+++ RestFullService/IServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestFullService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServer" in both code and config file together.
+    [ServiceContract]
+    public interface IServer
+    {
+        [OperationContract]
+        void DoWork();
+        [OperationContract]
+        [WebGet(RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string Samp();
+    }
+}
---
+++ RestFullService/Server.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RestFullService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Server" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Server.svc or Server.svc.cs at the Solution Explorer and start debugging.
+    public class Server : IServer
+    {
+        public string Samp()
+        {
+            return "Hello World";
+        }
+
+        public void DoWork()
+        {
+        }
+    }
+}
